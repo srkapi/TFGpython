0b6c553408c18235125311365ca238acdbfc85b6
sr.kapi <sr.kapi@MacBook-Pro-de-srkapi.local>
2016-03-19 15:53:20 +0100

    add boopstrap

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -3,6 +3,9 @@
 
 
 import os
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 ####CHECK THIS OUT####
@@ -54,7 +57,8 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -65,19 +69,16 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static') # Used only during production deployment.
+STATIC_URL = '/static/' # I'm a minimalist...
 
+
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__)+'/..')  # The '/..' is needed to work with Django 1.4+, remove for ol
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'static'),
 )
-
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
