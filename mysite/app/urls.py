07057d39b2cd6f5ecc9cd92678c7fdb16737683b
srkapi <albert@kapi.com>
2016-05-16 19:59:57 +0200

    update version python and created first test

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -6,7 +6,7 @@ from app.views import *
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^measureadd/$', measureAdd, name='urlname'),
-    url(r'^measure/$', measure, name='data'),
+    url(r'^measure/$', measure, name='measure'),
     url(r'^login/$', login_user , name='login'),
     url(r'^logout/$', logout_user, name='logout'),
     url(r'^user/$', list_user, name='user'),
