075664394705f60890cda111b8c853fcea19c43a
srkapi <albert@kapi.com>
2016-06-06 21:57:15 +0200

    add angular in measure.html, create form and icon successfull

--- mysite/fileUpload/sckech.ino
+++ mysite/fileUpload/sckech.ino
@@ -1,19 +1,4 @@
-Create view file 
-	- html (successful)
-	- index.py (successful) 
-	- create dao (successful)
-	- mongo (successful)
-	- save file in directory (successful)
-Create start and end measure
-	- create HTML (successful)
-	- modify model 
-	- modify index.py
-	- create test mock
-Create autobahn Arduino tun
-Create api REST arduino .ino
-read api rest from python to ico
-send data from python to server
-logical test business
-create index memory
-create functional test
-create technical design
+show collections --> muestra las colecciones de mongo 
+
+
+76133529x
